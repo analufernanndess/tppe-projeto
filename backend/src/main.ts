b903c31771e023025db78a65e1f3fc2ce4b42401
analiuza123 <analiuza123@gmail.com>
2025-07-19 07:58:00 +0530

    ajustando backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
